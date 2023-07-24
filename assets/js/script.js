9430687648fcbe69107350d845ee30fb49c166ea
Florian Kreuk <fkreuk@cartermario.com>
2023-07-24 16:28:24 -0400

    working on js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,20 @@
-var city;
+var geocodeURL = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=832cf34605e4ecca0f923b6ee27d32a9'
+var weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=832cf34605e4ecca0f923b6ee27d32a9'
+
+var country = "US"
+var state = "";
+var city = "";
+
+
 const searchBtn = document.querySelector(".button");
 searchBtn.addEventListener('click', function() {
-    city = "input";
+    city = "Raleigh";
     console.log(city);
     getWeather();
 })
 
 
 function getWeather () {
-    fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city +"&limit=1&appid=832cf34605e4ecca0f923b6ee27d32a9") 
-}
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q=Raleigh&limit=1&appid=832cf34605e4ecca0f923b6ee27d32a9") 
+};
+
