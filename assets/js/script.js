3423bd014c79fb7a6ed9fd377aae69502ba4307d
Florian Kreuk <fkreuk@cartermario.com>
2023-07-24 16:43:53 -0400

    Added some additional js to try to get the weather API to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-var geocodeURL = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=832cf34605e4ecca0f923b6ee27d32a9'
-var weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=832cf34605e4ecca0f923b6ee27d32a9'
+// var geocodeURL = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=832cf34605e4ecca0f923b6ee27d32a9'
+// var weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=832cf34605e4ecca0f923b6ee27d32a9'
 
 var country = "US"
 var state = "";
@@ -8,13 +8,22 @@ var city = "";
 
 const searchBtn = document.querySelector(".button");
 searchBtn.addEventListener('click', function() {
-    city = "Raleigh";
+    city = document.getElementById("cityId").value;
     console.log(city);
     getWeather();
 })
 
 
 function getWeather () {
-    fetch("http://api.openweathermap.org/geo/1.0/direct?q=Raleigh&limit=1&appid=832cf34605e4ecca0f923b6ee27d32a9") 
-};
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q="+ city + "&limit=1&appid=832cf34605e4ecca0f923b6ee27d32a9") 
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+       var weather = data.weather[0].icon;
+        console.log(weather);
+        });
+    };
+    
+        
 
