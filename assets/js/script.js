b3b33d97d1927df39ffecc52f73fbfb6da5b0ef4
Florian Kreuk <fkreuk@cartermario.com>
2023-07-25 13:24:05 -0400

    added js to store searches in local storage with key of searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,9 @@
-// var geocodeURL = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=832cf34605e4ecca0f923b6ee27d32a9'
-// var weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=832cf34605e4ecca0f923b6ee27d32a9'
-
-var country = "US"
-var state = "";
-var city = "";
-
-
 const searchBtn = document.querySelector(".button");
 searchBtn.addEventListener('click', function() {
     city = document.getElementById("cityId").value;
-    console.log(city);
+    addtolocalstorage();
     getWeather();
+    addtoULlist();
 })
 
 
@@ -25,5 +18,21 @@ function getWeather () {
         });
     };
     
-        
 
+
+// utilized https://www.youtube.com/watch?v=2hJ1rTANVnk for the local storage piece. The below code adds the user input cities after a click event into an array with a key of "searches".
+
+function addtolocalstorage () {
+    console.log(city);
+    var new_data = city;
+    if(localStorage.getItem('searches') == null){
+        localStorage.setItem('searches','[]');
+    };
+    var old_data = JSON.parse(localStorage.getItem('searches'));
+    old_data.push(new_data);
+    localStorage.setItem('searches',JSON.stringify(old_data));
+};
+ 
+function addtoULlist () {
+
+}
